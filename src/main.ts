6eaa9512c018261ed449fc37c6076bafb894e124
argon1025 <argon1025@gmail.com>
2024-01-29 20:05:58 +0900

    feat: Swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Muzi API')
+    .setDescription('무지 백엔드 서비스')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'Muzi API' });
+
   await app.listen(3000);
 }
 bootstrap();
