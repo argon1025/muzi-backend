592fc9e5ce5298ccb7674585b020a30e62b41914
argon1025 <argon1025@gmail.com>
2024-02-02 22:48:06 +0900

    feat: parsing-event 인터페이스 정의

---
+++ src/library/parsing-event/type/parsing-event.interface.ts
@@ -0,0 +1,69 @@
+export const PARSING_EVENT_SERVICE = Symbol('PARSING_EVENT_SERVICE');
+/**
+ * 외부 객체와 소통을 위한 EventService 인터페이스
+ */
+export namespace IParsingEventService {
+  export interface Base {
+    /** 이벤트 생성 */
+    createEvent(options: CreateEventOptions): Promise<void>;
+    /** 이벤트 조회 및 점유처리 */
+    getEvent(options: GetEventOptions): Promise<GetEventResult>;
+    /** 이벤트 작업 상태 수정 */
+    modifyEventStatus(options: ModifyEventStatusOptions): Promise<void>;
+    /** 이벤트 처리 로그 생성 */
+    createLog(options: CreateLogOptions): Promise<void>;
+  }
+
+  export interface CreateEventOptions {
+    /** 이벤트 타입 */
+    eventType: EventType;
+    /** 이벤트 메시지 */
+    eventMessage: string;
+  }
+
+  export interface GetEventOptions {
+    /** 이벤트 타입 */
+    eventType: EventType;
+  }
+
+  export interface GetEventResult {
+    /** 이벤트 아이디 */
+    id: string;
+    /** 이벤트 타입 */
+    eventType: EventType;
+    /** 이벤트 메시지 */
+    eventMessage: string;
+    /** 이벤트 생성일 */
+    createdAt: Date;
+  }
+
+  export interface ModifyEventStatusOptions {
+    /** 이벤트 아이디 */
+    eventId: string;
+    /** 이벤트 상태 */
+    eventStatus: EventStatus;
+  }
+
+  export interface CreateLogOptions {
+    /** 이벤트 아이디 */
+    eventId: string;
+    /** 로그 메시지 */
+    logMessage: string;
+  }
+
+  export enum EventStatus {
+    /** 대기 */
+    WAIT = 'WAIT',
+    /** 처리중 */
+    PROCESSING = 'PROCESSING',
+    /** 완료 */
+    DONE = 'DONE',
+    /** 실패 */
+    FAIL = 'FAIL',
+  }
+
+  export enum EventType {
+    /** 디너의 여왕 전체 페이지 */
+    DINNER_QUEEN_ALL_PAGE = 'DINNER_QUEEN_ALL_PAGE',
+  }
+}
