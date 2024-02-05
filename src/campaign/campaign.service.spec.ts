8aa3bddcdd058f24df0e9a633c05d92b9140ee63
argon1025 <argon1025@gmail.com>
2024-02-06 03:15:16 +0900

    feat: 캠페인 검색 조건 추가

--- src/campaign/campaign.service.spec.ts
+++ src/campaign/campaign.service.spec.ts
@@ -1,35 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ForbiddenException, NotFoundException } from '@nestjs/common';
 import { DateTime } from 'luxon';
 import { CampaignService } from './campaign.service';
-import { CAMPAIGN_REPOSITORY } from './type/campaign.repository.interface';
-import { CampaignRepository } from './repository/campaign.repository';
-import { USER_CAMPAIGN_REPOSITORY } from './type/user-campaign.repository.interface';
-import { UserCampaignRepository } from './repository/user-campaign.repository';
 import { PrismaService } from '../library/prisma/prisma.service';
 import { ERROR_CODE } from '../library/exception/error.constant';
 
 describe('CampaignService', () => {
   let campaignService: CampaignService;
-  let prismaService: PrismaService;
+  const prismaService: PrismaService = new PrismaService();
 
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        CampaignService,
-        { provide: CAMPAIGN_REPOSITORY, useClass: CampaignRepository },
-        { provide: USER_CAMPAIGN_REPOSITORY, useClass: UserCampaignRepository },
-        PrismaService,
-      ],
-    }).compile();
-
-    campaignService = module.get<CampaignService>(CampaignService);
-    prismaService = module.get<PrismaService>(PrismaService);
+    campaignService = new CampaignService(prismaService);
   });
 
   afterEach(async () => {
     await prismaService.$transaction([prismaService.userCampaign.deleteMany(), prismaService.campaign.deleteMany(), prismaService.user.deleteMany()]);
-    jest.clearAllMocks();
+    jest.restoreAllMocks();
   });
 
   afterAll(async () => {
@@ -37,7 +22,7 @@ describe('CampaignService', () => {
   });
 
   describe('findMany', () => {
-    it('조건에 맞는 캠페인을 찾고 결과를 리턴한다.', async () => {
+    it('전체 데이터를 요청 했을 경우.', async () => {
       // given
       // 시간 고정
       const fakeNowTime = DateTime.fromISO('2023-01-01T08:00:00Z').toJSDate(); // utc 기준
@@ -77,7 +62,7 @@ describe('CampaignService', () => {
           drawAt: null,
           endedAt: null,
           startedAt: null,
-          deletedAt: null,
+          deletedAt: fakeNowTime,
           updatedAt: fakeNowTime,
           createdAt: fakeNowTime,
         },
@@ -85,14 +70,15 @@ describe('CampaignService', () => {
       await prismaService.campaign.createMany({ data: campaignList });
 
       // when
-      const result = await campaignService.findMany({ address: 'address1', size: 10, page: 1 });
+      const result = await campaignService.findMany({ size: 10, page: 1 });
 
       // then
       expect(result.list.length).toBe(1);
       expect(result.list[0].id).toBe('campaign1');
       expect(result.total).toBe(1);
     });
-    it('조건에 맞지 않는 캠페인은 목록에서 제외한다.', async () => {
+
+    it('타이틀을 검색 했을 경우', async () => {
       // given
       // 시간 고정
       const fakeNowTime = DateTime.fromISO('2023-01-01T08:00:00Z').toJSDate(); // utc 기준
@@ -140,11 +126,126 @@ describe('CampaignService', () => {
       await prismaService.campaign.createMany({ data: campaignList });
 
       // when
-      const result = await campaignService.findMany({ address: 'address1', title: 'title2', size: 10, page: 1 });
+      const result = await campaignService.findMany({ title: 'title1', size: 10, page: 1 });
 
       // then
-      expect(result).toEqual({ list: [], total: 0 });
+      expect(result.total).toBe(1);
+      expect(result.list[0].title).toBe('title1');
     });
+
+    it('카테고리를 검색 했을 경우', async () => {
+      // given
+      // 시간 고정
+      const fakeNowTime = DateTime.fromISO('2023-01-01T08:00:00Z').toJSDate(); // utc 기준
+      // 캠페인 데이터 2건 등록
+      const campaignList = [
+        {
+          id: 'campaign1',
+          duplicateId: 'duplicateId1',
+          resourceProvider: 'resourceProvider1',
+          originUrl: 'originUrl1',
+          title: 'title1',
+          category: 'category1',
+          targetPlatforms: 'targetPlatforms1',
+          thumbnail: 'thumbnail1',
+          address: 'address1',
+          recruitCount: 1,
+          applyCount: 1,
+          drawAt: null,
+          endedAt: null,
+          startedAt: null,
+          deletedAt: null,
+          updatedAt: fakeNowTime,
+          createdAt: fakeNowTime,
+        },
+        {
+          id: 'campaign2',
+          duplicateId: 'duplicateId2',
+          resourceProvider: 'resourceProvider2',
+          originUrl: 'originUrl2',
+          title: 'title2',
+          category: 'category2',
+          targetPlatforms: 'targetPlatforms2',
+          thumbnail: 'thumbnail2',
+          address: 'address2',
+          recruitCount: 2,
+          applyCount: 2,
+          drawAt: null,
+          endedAt: null,
+          startedAt: null,
+          deletedAt: null,
+          updatedAt: fakeNowTime,
+          createdAt: fakeNowTime,
+        },
+      ];
+      await prismaService.campaign.createMany({ data: campaignList });
+
+      // when
+      const result = await campaignService.findMany({ category: 'category1', size: 10, page: 1 });
+
+      // then
+      expect(result.total).toBe(1);
+      expect(result.list[0].category).toBe('category1');
+    });
+
+    it('신청 진행중인 캠페인을 검색했을 경우', async () => {
+      // given
+      jest.useFakeTimers({ advanceTimers: true });
+      // 캠페인 등록일-마감일 설정
+      const startedAt = DateTime.fromISO('2023-01-02T00:00:00Z').toJSDate();
+      const endedAt = DateTime.fromISO('2023-01-04T00:00:00Z').toJSDate();
+      // 캠페인 데이터 등록
+      await prismaService.campaign.create({
+        data: {
+          id: 'campaign123',
+          duplicateId: 'duplicateId1',
+          resourceProvider: 'resourceProvider1',
+          originUrl: 'originUrl1',
+          title: 'title1',
+          category: 'category1',
+          targetPlatforms: 'targetPlatforms1',
+          thumbnail: 'thumbnail1',
+          address: 'address1',
+          recruitCount: 1,
+          applyCount: 1,
+          drawAt: null,
+          startedAt,
+          endedAt,
+          deletedAt: null,
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        },
+      });
+
+      // when
+      // 응모 시작일 전에 요청한 경우
+      jest.setSystemTime(DateTime.fromISO('2023-01-01T23:59:59Z').toJSDate());
+      const test1 = await campaignService.findMany({ hasAvailable: true, size: 10, page: 1 });
+
+      // // 응모 마감일 이후에 요청한 경우
+      jest.setSystemTime(DateTime.fromISO('2023-01-04T00:00:01Z').toJSDate());
+      const test2 = await campaignService.findMany({ hasAvailable: true, size: 10, page: 1 });
+
+      // 응모 시작일과 동일한 시간에 요청한 경우
+      jest.setSystemTime(DateTime.fromISO('2023-01-02T00:00:00Z').toJSDate());
+      const test3 = await campaignService.findMany({ hasAvailable: true, size: 10, page: 1 });
+
+      // // 응모 마감일과 동일한 시간에 요청한 경우
+      jest.setSystemTime(DateTime.fromISO('2023-01-04T00:00:00Z').toJSDate());
+      const test4 = await campaignService.findMany({ hasAvailable: true, size: 10, page: 1 });
+
+      // // 응모 가능일 내에 요청한 경우
+      jest.setSystemTime(DateTime.fromISO('2023-01-03T15:00:00Z').toJSDate());
+      const test5 = await campaignService.findMany({ hasAvailable: true, size: 10, page: 1 });
+
+      // then
+      expect(test1.total).toBe(0);
+      expect(test2.total).toBe(0);
+      expect(test3.total).toBe(1);
+      expect(test4.total).toBe(1);
+      expect(test5.total).toBe(1);
+    });
+
     it('삭제된 캠페인은 목록에서 제외한다', async () => {
       // given
       // 삭제된 캠페인 데이터 2건 등록
@@ -161,25 +262,6 @@ describe('CampaignService', () => {
           address: 'address1',
           recruitCount: 1,
           applyCount: 1,
-          drawAt: null,
-          endedAt: null,
-          startedAt: null,
-          deletedAt: new Date(),
-          updatedAt: new Date(),
-          createdAt: new Date(),
-        },
-        {
-          id: 'campaign2',
-          duplicateId: 'duplicateId2',
-          resourceProvider: 'resourceProvider2',
-          originUrl: 'originUrl2',
-          title: 'title2',
-          category: 'category2',
-          targetPlatforms: 'targetPlatforms2',
-          thumbnail: 'thumbnail2',
-          address: 'address2',
-          recruitCount: 2,
-          applyCount: 2,
           drawAt: null,
           endedAt: null,
           startedAt: null,
--- src/campaign/campain.module.ts
+++ src/campaign/campain.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../library/prisma/prisma.module';
 import { CampaignController } from './campaign.controller';
-import { CAMPAIGN_REPOSITORY } from './type/campaign.repository.interface';
 import { CampaignService } from './campaign.service';
-import { CampaignRepository } from './repository/campaign.repository';
-import { UserCampaignRepository } from './repository/user-campaign.repository';
 import { CAMPAIGN_SERVICE } from './type/campaign.service.interface';
-import { USER_CAMPAIGN_REPOSITORY } from './type/user-campaign.repository.interface';
 import { JwtUtilityModule } from '../library/jwt-utility/jwt-utility.module';
 
 @Module({
   imports: [PrismaModule, JwtUtilityModule],
   controllers: [CampaignController],
-  providers: [
-    { provide: CAMPAIGN_REPOSITORY, useClass: CampaignRepository },
-    { provide: USER_CAMPAIGN_REPOSITORY, useClass: UserCampaignRepository },
-    { provide: CAMPAIGN_SERVICE, useClass: CampaignService },
-  ],
+  providers: [{ provide: CAMPAIGN_SERVICE, useClass: CampaignService }],
 })
 export class CampaignModule {}
